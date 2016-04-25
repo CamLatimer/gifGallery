0a58969739d281630812275a018a4e9d48f85ed6
Cam <camlatimer@gmail.com>
2016-04-25 09:07:01 -0400

    main app file(index.js) and wiring, and dependencies, etc are up

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var Schema = require('./db/models');
+var mongoose = require('mongoose');
+var parser = require('body-parser');
+
+app = express();
+
+var HypeRef = Schema.HypeRef;
+var Comment = Schema.Comment;
+
+app.set('port', process.env.PORT || 3001);
+
+app.use(express.static(__dirname + '/public'));
+app.use(parser.urlencoded({extended: true}));
+// app.use(parser.json({extended: true}));
+
+
+// get all refs
+app.get('/api/refs', function(req, res){
+  HypeRef.find({}).then(function(refs){
+    res.json(refs);
+  })
+});
+// get a single ref
+app.get('/api/refs/:_id', function(req, res){
+  HypeRef.findOne({_id: req.params._id}).then(function(ref){
+    res.json(ref);
+  });
+});
+// create a ref
+app.post('/api/refs', function(req, res){
+  console.log(req.body);
+  HypeRef.create(req.body).then(function(err, ref){
+    if (err) {
+      console.log(err);
+    }
+    res.json(ref);
+  });
+});
+
+
+app.listen(app.get('port'), function(){
+  console.log('server up');
+});
