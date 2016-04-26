19d755b4b0c72ea839cf6ed8cbd10dac01228f9b
Cam <camlatimer@gmail.com>
2016-04-26 10:54:52 -0400

    adds a method to call giphy api

--- public/js/apiService.js
+++ public/js/apiService.js
@@ -8,6 +8,15 @@
       return $http.get('http://localhost:8080/api/refs')
       .then(callback);
     };
+    this.getGiphs = function(callback){
+      return $http.get('http://api.giphy.com/v1/gifs/random?q=cat&limit=201&api_key=dc6zaTOxFJmzC')
+      .then(callback);
+    };
   });
 
 })();
+
+// giphy public beta key dc6zaTOxFJmzC
+
+
+// gangsta kitten id:"l4KhYXYuv0HH0AExy"
