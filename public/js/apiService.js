f0d488dafa8c2d01cd357f6051532468bb125bbc
Cam <camlatimer@gmail.com>
2016-04-28 17:47:11 -0400

    cleans up unnessary comments

--- public/js/apiService.js
+++ public/js/apiService.js
@@ -14,15 +14,3 @@
     };
   });
 })();
-
-// giphy public beta key dc6zaTOxFJmzC
-// gangsta kitten id:"l4KhYXYuv0HH0AExy"
-
-// imgur client id feecb4475463d6d
-// imgur client secret 34e63c1274a1eaa76cc20ffa6df35e9368ac7c8a
-
-// deviant art client ed 4628
-// deviant art shared key 9b89e4d4fc6899a00cfa3d629d056fc6
-
-// flicker key bf937da79b9e9c807250789f79452a82
-// flickr secrete e996d35c2e99460b
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -36,8 +36,6 @@
 
     $scope.adder = function(){
       var ref = $scope.ref;
-      // ref.img_url = $scope.giph.image_original_url;
-      // ref.og_still_url = $scope.giph.fixed_height_small_still_url;
       ref.critiques = [{body: ref.critique}];
       $http.post('/api/refs', ref)
       .then(function(response){
@@ -47,15 +45,6 @@
 
     $scope.loadGiph();
 
-    // $http.get('https://api.imgur.com/3/gallery/t/items/cat', {headers: {'Authorization': 'Client-ID feecb4475463d6d'}})
-    // .then(function(response){
-    //   console.log('imgur-data: ' + response.data);
-    // })
-
-    // $http.get('http://api.lordofthememe.com/v1/posts/random.json')
-    // .then(function(response){
-    //   console.log(response.data);
-    // })
   })
   .controller('ShowCtrl', function($scope, $http, $stateParams, HypeRef){
     var refId = $stateParams.id;
@@ -72,8 +61,3 @@
     });
   })
 })();
-
-// sndcld client id 4f2b9615c8783056a2eae41eba103c48
-// client secret 1c1fb12b48934406245e0a82f59b2518
-// last.fm client id cb00639c97833039c6767c12ff335cf8
-// Shared secret	1a50195b4b3af4a5a597c1917dade5dd
