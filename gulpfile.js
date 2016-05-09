84bebee32ebc51d95211d4f8f6c36cf151707eed
Cam <camlatimer@gmail.com>
2016-05-09 16:41:45 -0400

    using browser synce and nodemon to reload app and refresh browser

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require("gulp-sass");
 var watch = require('gulp-watch');
+var nodemon = require('gulp-nodemon');
+var browserSync = require("browser-sync").create();
 
 gulp.task('sass', function () {
   //if other tasks depend on this task, it must return something
@@ -13,8 +15,19 @@ gulp.task('watch', function () {
   gulp.watch('./public/style/style.scss', ['sass']);
 });
 
+gulp.task('start', function () {
+  browserSync.init({
+    proxy: "localhost:8080",
+    files: ['**/*.css', '**/*.html']
+  });
+  nodemon({
+    script: 'index.js',
+    ext: 'js',
+    env: { 'NODE_ENV': 'development' }
+  }).on('start', function(){
+    browserSync.reload('**/*.css', '**/*.html');
+})
 
-
-gulp.task('default', ['sass', 'watch'], function(){
+gulp.task('default', ['start', 'sass', 'watch'], function(){
   console.log('gulp is on...');
 })
