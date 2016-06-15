223e643479602aa8e288c8fd09bf927d284c230c
Cam <camlatimer@gmail.com>
2016-06-15 19:47:53 -0400

    adds a placeholder for when the page is loading slowly

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -7,19 +7,46 @@
       // set variable in this scope, so the items can be accessed outside of loadGifs();
       $scope.catalogue;
       // limit of items that can be shown at any time.
-      $scope.gifLimit = 10;
+      $scope.gifLimit = 12;
       $scope.gifTotal;
+      var intervl;
+
+      function blink(){
+        $scope.loading = true;
+        var status = 0;
+        var holder = document.querySelector('.loading-holder');
+        intervl = setInterval(function() {
+          if (status === 0){
+            holder.style.opacity = 0;
+            console.log('light');
+            status = 1;
+          } else if(status === 1) {
+            holder.style.opacity = 1;
+            console.log('dark');
+            status = 0;
+          }
+        }, 300);
+      }
+      function stopBlink(){
+        $scope.loading = false;
+        clearInterval(intervl);
+        console.log('stopped');
+      }
 
       // function that grabs gifs from API
       $scope.loadGifs = function(){
+        //when there's no data, run the interval
+        blink();
         Gif.getGifs(function(response){
         $scope.catalogue = response.data;
+        // when there's data, clear the interval
+        stopBlink();
         $scope.gifTotal = $scope.catalogue.length;
         });
       }
       // function that loads specified amount of items
       $scope.loadMore = function() {
-        $scope.gifLimit += 10;
+        $scope.gifLimit += 12;
         // makes load more button disapear if all the gifs are loaded
         if($scope.gifLimit >= $scope.gifTotal) {
           $scope.showBtn = false;
@@ -27,6 +54,7 @@
           $scope.showBtn = true;
         }
       }
+
       // makes the call to load the gifs
       $scope.loadGifs();
       // makes load more button disapear if all the gifs are loaded
@@ -37,6 +65,9 @@
       }
   })
   .controller('GifCtrl', function($scope, $state, Gif, $http){
+
+    $scope.loading = true;
+
     $scope.reload = function(){
       $state.reload('gif');
     };
@@ -44,6 +75,9 @@
     $scope.loadGif = function(){
       Gif.getGiphys(function(response){
         var giphy = response.data;
+        if(giphy){
+          $scope.loading = false;
+        }
         console.log(giphy);
         return $scope.giphy = giphy.data;
       });
@@ -67,6 +101,7 @@
         .then(function(response){
           console.log(ref.critiques);
         });
+        $scope.loading = true;
         $state.go('catalogue');
       }
     };
